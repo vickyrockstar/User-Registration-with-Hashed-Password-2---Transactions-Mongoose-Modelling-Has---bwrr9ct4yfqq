4c73c2a5edecb845c7299d047ec441398abe13ba
vighnesh shetty <116479689+vickyrockstar@users.noreply.github.com>
2023-05-21 22:43:11 +0530

    Update userControllers.js

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,29 +1,76 @@
 const users   =require("../models/user.js");
 
+const bcrypt = require('bcrypt');
 
 /*
+
 Post request json file structure
 
-
     obj =  {
+
         "name":name,
+
         "email":email,
-        "password": password,
-        "DOB": DOB
+
+        "password": password
+
     }
 
  */
 
 //You need to complete the route of user register
-//you need to register the user and return the id assign to the user.
-//the password you save in database should be hashed using bcrypt libary.
-//you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
-//to look the user schema look ../models/user.js
 
+//you need to register the user and return the id assign to the user.
+
+//the password you save in database should be hashed using bcrypt libary.
+
+//you will get error if user mail allready exist in that case you need to return 404 status with err message that you get.
+
+//to look the user schema look ../models/user.js
 
 const registerUser =async (req, res) => {
 
-    //write your code here
+    //Write you code here
+
+     const name = req.body.name;
+
+    const email  = req.body.email;
+
+    const password = req.body.password;
+
+    const DOB = req.body.DOB;
+
+    // Generate a salt to be used for hashing
+
+    const saltRounds = 10;
+
+    const salt = bcrypt.genSaltSync(saltRounds);
+
+    const hashedPassword = bcrypt.hashSync(password, salt);
+
+    var newuser = {
+
+        "name":name,
+
+        "email":email,
+
+        "password": hashedPassword,
+
+        "DOB":DOB
+
+    };
+
+    users.create(newuser).then((user) => {
+
+        res.send(user._id);
+
+    })
+
+    .catch((error) => {
+
+        res.status(404).send(error.message);
+
+    });
 
 }
 
